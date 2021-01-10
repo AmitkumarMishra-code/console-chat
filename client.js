b3af51f29351352ea96042e55f13df58bc6fe511
Amitkumar Mishra <71927649+AmitkumarMishra-code@users.noreply.github.com>
2021-01-10 23:04:30 +0530

    Update client.js

--- client.js
+++ client.js
@@ -5,26 +5,15 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 // write your code here
 rl.question("What is your name?\n> ", (name) => {
     const socket = io('http://127.0.0.1:3000')
-    socket.on('connect', () => {
-        console.log('Successfully connected to server')
-        message(socket, name)
-    })
-    socket.on('disconnect', ()=>{
-        console.log('Connection lost...')
-        message(socket, name)
-    })
-    socket.on('simple chat message', (incoming) => {                // I made changes here. I removed the if condition where I was checking for the
-        console.log(incoming)                                       // name of the user sending the message.
-        message(socket, name)
-    })
+    socket.on('connect', () => message(socket, name,'Successfully connected to server'))
+    socket.on('disconnect', () => message(socket, name, 'Connection lost...'))
+    socket.on('simple chat message', (incoming) => message(socket, name, incoming))
 })
-function message(socket, name){
+function message(socket, name, displayMessage){
+    displayMessage!=undefined && console.log(displayMessage)
     rl.question("> ", (msg)=> {
         socket.emit("simple chat message", name + ' says: ' + '"' + msg + '"')
         console.log('Sending Message : "' + msg + '"' )
         message(socket, name);
     })
 }
-
-
-
